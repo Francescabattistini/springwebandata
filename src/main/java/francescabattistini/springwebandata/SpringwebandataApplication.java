950cc1c0c320e18a5fc83dcf63cf228cf6adfa0e
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-30 16:09:27 +0100

    Initial commit

---
+++ src/main/java/francescabattistini/springwebandata/SpringwebandataApplication.java
@@ -0,0 +1,13 @@
+package francescabattistini.springwebandata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebandataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebandataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/francescabattistini/springwebandata/SpringwebandataApplicationTests.java
@@ -0,0 +1,13 @@
+package francescabattistini.springwebandata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebandataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
